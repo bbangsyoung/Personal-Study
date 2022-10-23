26f61e00d934e3ad66f4166f5e56ba8ebb28d138
bbangdi <bbangord@gmail.com>
2022-10-24 00:48:27 +0900

    Spring - 검색기능추가구현

--- BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
+++ BoardWeb/src/main/java/com/springbook/view/board/BoardController.java
@@ -3,18 +3,24 @@ package com.springbook.view.board;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
+import com.springbook.biz.board.BoardService;
 import com.springbook.biz.board.BoardVO;
 import com.springbook.biz.board.impl.BoardDAO;
 
 @Controller
 @SessionAttributes("board")
 public class BoardController {
+	
+	@Autowired
+	private BoardService boardService;
+	
 
 	// 검색 조건 목록 설정
 		@ModelAttribute("conditionMap")
@@ -28,52 +34,59 @@ public class BoardController {
 	
 	// 글등록
 	@RequestMapping(value = "/insertBoard.do")
-	public String insertBoard(BoardVO vo, BoardDAO boardDAO) {
+	public String insertBoard(BoardVO vo) {
 		System.out.println("글 등록 처리");
 
-		boardDAO.insertBoard(vo);
+		boardService.insertBoard(vo);
 		return "redirect:getBoardList.do";
 	}
 
 	// 글수정
 	@RequestMapping("/updateBoard.do")
-	public String updateBoard(@ModelAttribute("board") BoardVO vo, BoardDAO boardDAO) {
+	public String updateBoard(@ModelAttribute("board") BoardVO vo) {
 		System.out.println("글번호 : " + vo.getSeq());
 		System.out.println("제목: " + vo.getTitle());
 		System.out.println("작성자 : " + vo.getWriter());
 		System.out.println("ㄴㅐ용 : " + vo.getContent());
 		System.out.println("등록일 : " + vo.getRegDate());
 		System.out.println("조회수 : " + vo.getCnt());
-		boardDAO.updateBoard(vo);
+		boardService.updateBoard(vo);
 		return "getBoardList.do";
 	}
 
 	// 글삭제
 	@RequestMapping("/deleteBoard.do")
-	public String deleteBoard(BoardVO vo, BoardDAO boardDAO) {
+	public String deleteBoard(BoardVO vo) {
 		System.out.println("글 삭제 처리");
 
-		boardDAO.deleteBoard(vo);
+		boardService.deleteBoard(vo);
 		return "getBoardList.do";
 	}
 
 	// 글 상세조회
-	@RequestMapping("/getBoard.do")
-	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
-		System.out.println("글 상세 조회 처리");
-
-		model.addAttribute("board", boardDAO.getBoard(vo));
+/*	@RequestMapping("/getBoard.do")
+	public String getBoard(BoardVO vo, Model model) {
+		model.addAttribute("board", boardService.getBoard(vo)); //Model 정보 저장
 		return "getBoard.jsp";
 	}
-
+*/
+	@RequestMapping("/getBoard.do")
+	public String getBoard(BoardVO vo, BoardDAO boardDAO, Model model) {
+		model.addAttribute("board", boardDAO.getBoard(vo)); //Model 정보 저장
+		return "getBoard.jsp"; // View 정보 저장
+	}
+	
 	
 
 	// 글 목록 검색
 	@RequestMapping("/getBoardList.do")
-	public String getBoardList(BoardVO vo, BoardDAO boardDAO, Model model) {
-
-		// Model 정보 저장
-		model.addAttribute("boardList", boardDAO.getBoardList(vo));
+	public String getBoardList(BoardVO vo, Model model) {
+		//Null check
+		if(vo.getSearchCondition() ==null) vo.setSearchCondition("TITLE");
+		if(vo.getSearchKeyword() ==null) vo.setSearchKeyword("");
+		
+		
+		model.addAttribute("boardList", boardService.getBoardList(vo));
 		return "getBoardList.jsp"; // View 이름 리턴
 	}
 
